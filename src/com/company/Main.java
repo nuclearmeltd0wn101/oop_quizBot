1a7210d56b4ed3398540caf27aeff947c796418f
nm101 <melnikovsm2@gmail.com>
2021-10-03 18:52:41 +0500

    Implement Telegram ChatBot Wrapper

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,15 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        var env = System.getenv();
+        var token = env.getOrDefault("tgBotToken", "");
+        if ("".equals(token)) {
+            System.err.println("Telegram Bot Token not submitted");
+            return;
+        }
+
         var botLogic = new TestLogic();
-        var bot = new ConsoleChatWrapper(botLogic);
+        var bot = new TelegramBotWrapper(botLogic, token);
         bot.run();
     }
 }
