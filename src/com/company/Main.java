078525f8bda6b3a73867ce54c674e5f6dfea6f8e
nm101 <melnikovsm2@gmail.com>
2021-10-18 17:01:09 +0500

    Multiuser basement (quick & dirty) implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,17 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-//         var env = System.getenv();
-//         var token = env.getOrDefault("tgBotToken", "");
-//         if ("".equals(token)) {
-//             System.err.println("Telegram Bot Token not submitted");
-//             return;
-//         }
-// 
-//         var bot = new TelegramBotWrapper(botLogic, token);
+        var env = System.getenv();
+        var token = env.getOrDefault("tgBotToken", "");
+        if ("".equals(token)) {
+            System.err.println("Telegram Bot Token not submitted");
+            return;
+        }
 
         var questions = QuestionsParser.fromTextFile("quiz_questions.txt", "\\*");
-        var botLogic = new QuizLogic(questions);
-        var bot = new ConsoleChatWrapper(botLogic);
+        var db = new QuizDBRAM();
+        var botLogic = new QuizLogic(questions, db);
+        var bot = new TelegramBotWrapper(botLogic, token);
 
         bot.run();
     }
--- src/com/company/QuizLogic.java
+++ src/com/company/QuizLogic.java
@@ -5,6 +5,8 @@ import java.util.Random;
 
 public class QuizLogic implements IChatBotLogic {
 
+    private static final int giveUpCountRequired = 3;
+
     private static final String greetMessagePM
             = "Привет, я - Викторина-бот!\nНапиши \"вопрос\" и я задам тебе вопрос";
     private static final String greetMessageChat
@@ -17,48 +19,103 @@ public class QuizLogic implements IChatBotLogic {
     private static final String messageWrongAnswer = "Вы не угадали!";
 
     private final ArrayList<QuizQuestion> questions;
-    private boolean isQuestion_present;
-    private int currentQuestionNumber;
     private final Random rand;
-
-    public QuizLogic(ArrayList<QuizQuestion> questions)
+    private IQuizDB db;
+    public QuizLogic(ArrayList<QuizQuestion> questions, IQuizDB db)
     {
         this.questions = questions;
         rand = new Random();
+        this.db = db;
     }
 
     public ChatBotResponse handler(ChatBotEvent event) {
         if (!event.isPrivateChat && !event.isMentioned) // ignore public chat w\o mention
             return null;
 
-        if ("/help".equals(event.message))
+        var state = db.getState(event.chatId);
+
+        if (event.message.contains("/score"))
+        {
+            var table= db.getScoreTable(event.chatId);
+            if (db.getScoreTable(event.chatId) == null)
+                return event.toResponse("the score table is empty");
+
+            var sb = new StringBuilder();
+            for (QuizScore item:table)
+            {
+                sb.append(item);
+                sb.append("\n");
+            }
+            return event.toResponse(sb.toString());
+
+        }
+
+        if (event.message.contains("/help"))
             return event.toResponse(
                     !event.isPrivateChat
                             ? greetMessageChat
                             : greetMessagePM);
 
         if (event.message.contains("вопрос")) {
-            currentQuestionNumber = rand.nextInt(questions.size());
+            if (state != 0)
+                return event.toResponse("Вопрос уже был задан,ожидаю ответ");
 
-            var question = questions.get(currentQuestionNumber);
+            var questionId = rand.nextInt(questions.size());
+            db.setQuestionId(event.chatId, questionId);
 
-            var text = String.format(messageFormatQuestion,
-                    question.question,
-                    question.answerHintFirstLetter(),
-                    question.answerHintLength()
-            );
+            db.setState(event.chatId,1);
+            var question = questions.get(questionId);
+            var text = question.question;
 
-            isQuestion_present = true;
             return event.toResponse(text);
         }
-        if (isQuestion_present) {
-            var question = questions.get(currentQuestionNumber);
+
+        if (state != 0) {
+            var questionId= Math.toIntExact(db.getQuestionId(event.chatId));
+            var question = questions.get(questionId);
+
             if (question.validateAnswer(event.message.toLowerCase())) {
-                isQuestion_present = false;
+                db.setState(event.chatId,0);
+                db.scoreIncrement(event.chatId,event.senderId);
                 return event.toResponse(messageRightAnswer);
             }
-            else
-                return event.toResponse(messageWrongAnswer);
+
+            var failureCount = state % 7 - 1;
+            var giveUpCount = state / 7;
+            var response = "";
+
+            if (event.message.contains("сдаюсь")) {
+                if (giveUpCount < giveUpCountRequired - 1) {
+                    giveUpCount++;
+                    response = String.format("Напишите \"сдаюсь\" ещё %s раз(-а)",
+                            giveUpCountRequired - giveUpCount);
+                }
+                else
+                {
+                    failureCount = giveUpCount = 0;
+                    response = "Вопрос снят. Напишите \"вопрос\", чтобы получить новый вопрос.";
+                }
+
+            }
+            else {
+                var sb = new StringBuilder("Вы не угадали.");
+                if (failureCount < 5)
+                    failureCount++;
+
+                if (failureCount >= 3)
+                    sb.append(String.format("\n\nПодсказка №1: Ответ начинается с буквы \"%s\"",
+                            question.answerHintFirstLetter()));
+
+                if (failureCount == 5)
+                    sb.append(String.format("\nПодсказка №2: Длина ответа - %s символов",
+                            question.answerHintLength()));
+
+                response = sb.toString();
+            }
+            var newState = 1 + failureCount + giveUpCount * 6;
+            db.setState(event.chatId, newState);
+
+            return event.toResponse(response);
         }
 
         return event.toResponse(messageHelpHint);
