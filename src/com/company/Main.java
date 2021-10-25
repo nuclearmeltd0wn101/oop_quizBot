ea460cabd79c147f37b710e87635214d92ec4bb1
nm101 <melnikovsm2@gmail.com>
2021-10-25 19:53:00 +0500

    Teammate fckup fix attempt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,15 @@ public class Main {
     public static void main(String[] args) {
         var env = System.getenv();
         var token = env.getOrDefault("tgBotToken", "");
+        var dbPath = env.getOrDefault("dbPath", "QuizBot.sqlite");
         if ("".equals(token)) {
             System.err.println("Telegram Bot Token not submitted");
             return;
         }
 
         var questions = QuestionsParser.fromTextFile("quiz_questions.txt", "\\*");
-        var db = new QuizDBRAM();
-        db.setUserName(382139175,"aaa");
-        db.setUserName(683545096,"bbbaaaaaaa");
-        for (var i=0;i<10;i++)
-        {
-            db.scoreIncrement(-528640224,382139175);
-            db.scoreIncrement(-528640224,683545096);
-        }
+        var db = new QuizDBSQLite(dbPath);
+
         var botLogic = new QuizLogic(questions, db);
         var bot = new TelegramBotWrapper(botLogic, token);
         bot.run();
